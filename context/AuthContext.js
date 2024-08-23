12b22cd7f8ab8a6fb73f52fda3213d348ec13314
koladeolukoya <koladeolukoya@gmail.com>
2024-08-23 09:09:32 +0100

    feat: api call to fetch users now works

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -2,43 +2,27 @@ import { createContext, useContext, useState, useEffect } from 'react';
 import { signInApi } from '@/utils/signInApi';
 import { signOutApi } from '@/utils/signOutApi';
 import { userDataApi } from '@/utils/getUserDataApi';
+import axios from 'axios';
 
 const AuthContext = createContext(undefined);
 
 export const AuthProvider = ({ children }) => {
-  const [userId, setUserId] = useState(null);
+  
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const storedUserId = sessionStorage.getItem('userId');
-    if (storedUserId) {
-      setUserId(storedUserId);
-    }
-  }, []);
-
-  useEffect(() => {
-    const getUserData = async () => {
-      if (userId) {
-        try {
-          const userData = await userDataApi(userId);
-          setUser(userData);
-        } catch (error) {
-          console.error('Get user failed', error);
-        } finally {
-          setLoading(false);
-        }
-      }
-    };
-
-    getUserData();
-  }, [userId]);
 
   const signIn = async (email, password) => {
     try {
       const userId = await signInApi(email, password);
+      console.log('User ID: ', userId);
+      document.cookie = `sessionId=${userId}; path=/`;
+
       sessionStorage.setItem('userId', userId);
-      setUserId(userId);
+
+      const res = await axios.post('http://localhost:8080/users/editUsers', { id: userId }, { withCredentials: true });
+      console.log("User: ", res.data.data);
+      const user = res.data.data;
+      setUser(user);
     } catch (error) {
       console.error('Sign in failed', error);
     }
@@ -49,14 +33,34 @@ export const AuthProvider = ({ children }) => {
       await signOutApi();
       sessionStorage.removeItem('userId');
       setUser(null);
-      setUserId(null);
     } catch (error) {
       console.error('Sign out failed', error);
     }
   };
 
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //       const userId = sessionStorage.getItem('userId');
+  //       console.log('User ID inside the get user fucntion: ', userId);
+  //     if (userId) {
+  //       console.log('User ID inside the get user fucntion: ', userId);
+  //       try {
+  //         const userData = await userDataApi(userId);
+  //         setUser(userData);
+  //         console.log('User data: ', userData);
+  //       } catch (error) {
+  //         console.error('Get user failed', error);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     }
+  //   };
+
+  //   getUserData();
+  // }, []);
+
   return (
-    <AuthContext.Provider value={{ user, userId, signIn, signOut, loading }}>
+    <AuthContext.Provider value={{ user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
--- utils/signOutApi.js
+++ utils/signOutApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const signOutApi = async () => {
   try {
-    const res = await axios.post('http://localhost:8080/auth/logout');
+    const res = await axios.get('http://localhost:8080/auth/logout', {}, { withCredentials: true });
     console.log("response: ", res);
     return res.data;
   } catch (error) {
