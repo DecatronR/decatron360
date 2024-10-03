999a8bafda95ff6ea0866a55ecb0dba704d9abcf
koladeolukoya <koladeolukoya@gmail.com>
2024-10-03 17:13:30 +0100

    feat: improved agent property page with agent name

--- app/agent-properties/[id]/page.jsx
+++ app/agent-properties/[id]/page.jsx
@@ -3,11 +3,30 @@ import React, { useEffect, useState } from "react";
 import AgentProperties from "@/components/AgentProperties/AgentProperties";
 import AgentPropertySearchForm from "@/components/AgentProperties/AgentPropertiesSearchForm";
 import { fetchUserProperties } from "@/utils/api/user/fetchUserProperties";
+import { fetchUserData } from "@/utils/api/user/fetchUserData";
 import { useParams } from "next/navigation";
 
 const AgentPropertiesPage = async () => {
   const { id } = useParams();
+  const [agentData, setAgentData] = useState(null);
   const [agentProperties, setAgentProperties] = useState([]);
+
+  useEffect(() => {
+    const handleFetchAgentData = async () => {
+      if (id) {
+        try {
+          const res = await fetchUserData(id);
+          console.log("agent data: ", res);
+          setAgentData(res);
+        } catch (error) {
+          console.log("Issues fetching agent details: ", error);
+        }
+      } else {
+        console.log("Could not fetch agent details, user id not found");
+      }
+    };
+    handleFetchAgentData();
+  }, [id]);
 
   useEffect(() => {
     const handleFetchAgentProperties = async () => {
@@ -25,10 +44,17 @@ const AgentPropertiesPage = async () => {
     };
     handleFetchAgentProperties();
   }, [id]);
+
   return (
     <>
       <section className="bg-primary-500 py-4">
         <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          {/* Display agent's name conditionally */}
+          {agentData && (
+            <h1 className="text-3xl font-bold text-white mb-4">
+              Properties Listed by {agentData.name}
+            </h1>
+          )}
           <AgentPropertySearchForm />
         </div>
       </section>
