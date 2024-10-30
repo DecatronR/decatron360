afa9ec333bfce803bab90079eff7c5f4c6c1ac03
koladeolukoya <koladeolukoya@gmail.com>
2024-10-30 11:01:35 +0100

    restrict search to nigeria

--- utils/helpers/getCoordinates.js
+++ utils/helpers/getCoordinates.js
@@ -2,18 +2,19 @@ import axios from "axios";
 
 const getCoordinates = async (address) => {
   const apiKey = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+  const country = "NG";
 
   const response = await axios.get(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
       address
-    )}.json?access_token=${apiKey}`
+    )}.json?access_token=${apiKey}&country=${country}`
   );
 
   if (response.data.features.length > 0) {
     const location = response.data.features[0].geometry.coordinates;
     return {
-      longitude: location[0], // longitude is the first element
-      latitude: location[1], // latitude is the second element
+      longitude: location[0],
+      latitude: location[1],
     };
   } else {
     throw new Error("Unable to get coordinates");
