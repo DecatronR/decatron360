68df7923ecc04647585cc2041d4089746531a46c
kolexstrings <koladeolukoya@gmail.com>
2024-12-06 23:12:40 +0100

    feat: added script to layour head

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@/assets/styles/globals.css";
 import "photoswipe/dist/photoswipe.css";
+import Script from "next/script";
 import ClientLayout from "@/components/ClientLayout";
 
 export const metadata = {
@@ -12,6 +13,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SPP14R22ER"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SPP14R22ER');
+          `}
+        </Script>
+      </head>
       <body className="min-h-screen flex flex-col">
         <ClientLayout>{children}</ClientLayout>
       </body>
