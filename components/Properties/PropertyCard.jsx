1e2dd9be6619523ecf4e901df9fec33960538daf
kolexstrings <koladeolukoya@gmail.com>
2024-12-29 12:31:50 +0100

    fix: favorite icon color

--- components/Properties/PropertyCard.jsx
+++ components/Properties/PropertyCard.jsx
@@ -52,7 +52,7 @@ const PropertyCard = ({ property, isFavorite, onToggleFavorite }) => {
             {isFavorite ? (
               <FaHeart className="text-red-500 text-2xl transition duration-300" />
             ) : (
-              <FaRegHeart className="text-white text-2xl transition duration-300" />
+              <FaRegHeart className="text-gray-300 text-2xl transition duration-300" />
             )}
           </button>
 
