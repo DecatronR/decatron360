7a6eb893a4da6ce96fc08757858c37d0ee855dc1
kolexstrings <koladeolukoya@gmail.com>
2025-01-23 13:59:40 +0100

    feat: duplexes

--- app/properties/categories/duplexes/page.jsx
+++ app/properties/categories/duplexes/page.jsx
@@ -72,7 +72,7 @@ const PropertiesDuplexes = () => {
     <>
       <section className="bg-primary-500 py-4">
         <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-white mb-4">Duplex</h1>
+          <h1 className="text-3xl font-bold text-white mb-4">Duplexes</h1>
           <PropertySearchForm />
         </div>
       </section>
