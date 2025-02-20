d8876e08b06a89bcdf3ebf9407a5931cbe47be9d
kolexstrings <koladeolukoya@gmail.com>
2025-02-20 16:52:30 +0100

    feat: referee schedule and referral booking api call

--- utils/api/relationship/fetchRefreeSchedule.js
+++ utils/api/relationship/fetchRefreeSchedule.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export const fetchUserTreeData = async (id) => {
+export const fetchRefreeSchedule = async (referralCode) => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const token = sessionStorage.getItem("token");
   try {
     const res = await axios.post(
-      `${baseUrl}/users/userTree`,
-      { id },
+      `${baseUrl}/mySchedule/fetchReferralSchedule`,
+      { referralCode },
       {
         withCredentials: true,
         headers: {
@@ -16,7 +16,7 @@ export const fetchUserTreeData = async (id) => {
     );
     return res.data.data;
   } catch (error) {
-    console.error("Error fetching user relationship tree data:", error);
+    console.error("Error fetching refree schedule:", error);
     throw error;
   }
 };
