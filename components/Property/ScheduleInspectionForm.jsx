ca47c6d0b7a26908e7c1a8e81d1b8360501591e3
kolexstrings <koladeolukoya@gmail.com>
2025-02-21 17:34:30 +0100

    feat: added place holder to inspection schduler date picker

--- components/Property/ScheduleInspectionForm.jsx
+++ components/Property/ScheduleInspectionForm.jsx
@@ -244,19 +244,22 @@ const ScheduleInspectionForm = ({ propertyId, agentId, referralCode }) => {
         )}
 
         <label className="block">
-          <span className="text-gray-700">Preferred Date & Time</span>
-          <DatePicker
-            selected={formData.date}
-            onChange={handleDateChange}
-            showTimeSelect
-            timeIntervals={60}
-            timeCaption="Time"
-            dateFormat="MMMM d, yyyy h:mm aa"
-            className="mt-1 block w-full rounded-md border-2 border-gray-300 focus:border-indigo-500 sm:text-sm px-4 py-2"
-            required
-            filterDate={(date) => isDateAvailable(date)} // Disable unavailable dates
-            filterTime={(time) => isTimeAvailable(time)} // Disable times not available
-          />
+          <span className="text-gray-700 block">Preferred Date & Time</span>
+          <>
+            <DatePicker
+              selected={formData.date}
+              onChange={handleDateChange}
+              showTimeSelect
+              timeIntervals={60}
+              timeCaption="Time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              className="mt-1 block w-full rounded-md border-2 border-gray-300 focus:border-indigo-500 sm:text-sm px-4 py-2"
+              required
+              filterDate={(date) => isDateAvailable(date)} // Disable unavailable dates
+              filterTime={(time) => isTimeAvailable(time)} // Disable times not available
+              placeholderText="Select a date and time"
+            />
+          </>
         </label>
         <button
           type="submit"
