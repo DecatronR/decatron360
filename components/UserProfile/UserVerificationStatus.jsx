6da8054efd8087476eb353c91a29ef7210f12187
kolexstrings <koladeolukoya@gmail.com>
2025-06-20 01:28:31 +0100

    merged shortlet

--- components/UserProfile/UserVerificationStatus.jsx
+++ components/UserProfile/UserVerificationStatus.jsx
@@ -43,7 +43,6 @@ const UserVerificationStatus = ({
             <button
               disabled
               className="bg-gray-400 text-white px-4 py-2 rounded-full cursor-not-allowed"
-              className="bg-gray-400 text-white px-4 py-2 rounded-full cursor-not-allowed"
             >
               Coming Soon
             </button>
