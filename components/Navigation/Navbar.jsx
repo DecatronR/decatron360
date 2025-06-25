a64d36a6ae04397ab6b0cf52e6d0d138bb296c68
kolexstrings <koladeolukoya@gmail.com>
2025-06-25 18:34:32 +0100

    misc

--- components/Navigation/Navbar.jsx
+++ components/Navigation/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
           {/* Mobile Profile Button (Only shows Sign Out) */}
           {user && (
             <div className="relative ml-3 block md:hidden">
-              <div className="absolute -left-12 top-1">
+              <div className="absolute -left-12">
                 <NotificationBell />
               </div>{" "}
               {/* Hides on md and larger */}
